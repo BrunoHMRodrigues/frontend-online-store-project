0b752533d4cddaf22e79c1728f8f51fe2936cc0f
CKafer <claudiokafer@hotmail.com>
2023-01-13 15:11:41 -0300

    corrigido problemas do 7 pro 8

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 
 class Card extends Component {
@@ -15,18 +16,25 @@ class Card extends Component {
   };
 
   render() {
-    const { price, title, thumbnail, productId } = this.props;
+    const { price, title, thumbnail, productId, data } = this.props;
     return (
-      <div data-testid="product">
-        <h3>{ title }</h3>
-        <img src={ thumbnail } alt={ title } />
-        <p>{ price }</p>
+      <div>
+        <Link
+          to={ `/product-detail/${data.id}` }
+          key={ data.id }
+          data-testid="product-detail-link"
+        >
+          <div data-testid="product">
+            <h3>{ title }</h3>
+            <img src={ thumbnail } alt={ title } />
+            <p>{ price }</p>
+          </div>
+        </Link>
         <button
           type="button"
           data-testid="product-add-to-cart"
           onClick={ this.handleCart }
           productId={ productId }
-          // data={ data }
         >
           Adicionar ao Carrinho
         </button>
